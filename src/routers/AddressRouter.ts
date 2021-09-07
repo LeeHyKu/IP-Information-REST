d1e8687048bc873208fdd840a52c96248491163c
Hyun-Ku Lee; cloud IDE <harry9111@naver.com>
2021-09-07 04:38:01 +0000

    First Commit

---
+++ src/routers/AddressRouter.ts
@@ -0,0 +1,62 @@
+/**
+ * REST Api, Server IP Address Information, Express.js, Node.js(TS4)
+ * Copyright (C) 2021  Hyun-Ku Lee
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import * as express from 'express';
+
+const ev_root = (req, res) => {
+    var ip     = req.socket.remoteAddress,
+        family = req.socket.remoteFamily,
+        port   = req.socket.remotePort;
+    
+    res
+        .status(200)
+
+    switch(req.params.rest?.toLowerCase?.()) {
+    case 'json':
+        res
+            .contentType('Application/json')
+            .send({
+                ip: ip,
+                family: family,
+                port: port
+            });
+        break;
+    case 'xml':
+        res
+            .contentType('text/xml')
+            .send(`<ipinfo><ip>${ip}</ip><family>${family}</family><port>${port}</port></ipinfo>`);
+    case 'text':
+    default:
+        res
+            .contentType('text/plain')
+            .send(`IP ADDRESS INFORMATION
+
+IP ADDRESS: ${ip}
+FAMILY:     ${family}
+PORT:       ${port}
+END`);
+        break;
+    }
+}
+
+const AddressRouter = express.Router();
+
+AddressRouter
+    .get('/', ev_root)
+    .get('/:rest', ev_root)
+
+export default AddressRouter;
